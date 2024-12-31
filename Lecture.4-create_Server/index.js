acd1076308ac5721a52254215b2ecda68552e70c
Bhumikarana75 <bhumikarana958@gmail.com>
2024-12-31 10:44:52 +0530

    Node JS

--- Lecture.4-create_Server/index.js
+++ Lecture.4-create_Server/index.js
@@ -1,33 +1,15 @@
 const http = require('http');
-const fs = require('fs');
 const port = 8000;
-const server = http.createServer((req, res) => {
-    let fileName = "";
 
-    switch (req.url) {
-        case '/':
-            fileName = './home.html';
-            break;
-        case '/ab':
-            fileName = './about.html';
-            break;
-        default:
-            fileName = null; // File not found for unknown routes
-    }
-    fs.readFile(fileName, (err, pagename) => {
-        if (err) {
-            console.log(`File is Not Found !`);
-            res.end('Internal Server Error');
-            return false;
-        }
-        res.end(pagename)
-    })
-})
-server.listen(port, (err) => {
-    if (!err) {
-        console.log(`server is start on port:- ${port}`);
-    } else {
-        console.error('Error starting server:', err);
-    }
+const server = http.createServer((req,res) => {
+    res.write(`<h1>Hello World</h1>`);
+    res.write(`<h4>Server is created with Node JS !</h4>`);
+    console.log("Request received");
+    res.end();
 })
 
+server.listen(port, (err) =>{
+    if(!err){
+        console.log("Server is running on port: ", port);
+    }
+})
---
+++ Lecture.5-Run-HTML-Page/index.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const port = 8080;
+const server = http.createServer((req, res) => {
+    let fileName = "";
+
+    switch (req.url) {
+        case '/':
+            fileName = './home.html';
+            break;
+        case '/ab':
+            fileName = './about.html';
+            break;
+        case '/con':
+            fileName = './contact.html';
+            break;
+        default:
+            fileName = null; // File not found for unknown routes
+    }
+    fs.readFile(fileName, (err, pagename) => {
+        if (err) {
+            console.log(`File is Not Found !`);
+            res.end();
+            return false;
+        }
+        res.end(pagename)
+    })
+})
+server.listen(port, (err) => {
+    if (!err) {
+        console.log(`server is start on port:- ${port}`);
+    } else {
+        console.error('Error starting server:', err);
+    }
+})
+
