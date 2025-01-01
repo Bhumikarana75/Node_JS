e46d43cda38dc07c64c08d0645c7b8ce376a6709
Bhumikarana75 <bhumikarana958@gmail.com>
2025-01-01 14:20:18 +0530

    Node JS

--- PR.2-ToDo_App_express/index.js
+++ PR.2-ToDo_App_express/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const path = require('path');
 app.set('view engine', 'ejs');
+app.use('/',express.static(path.join(__dirname,'assets')));
 app.use(express.urlencoded());
 let record = [];
 
@@ -19,11 +21,34 @@ app.get('/add', (req,res) => {
 app.post('/adduser',(req,res) => {
     const {name, phone} = req.body;
     let obj = {
+        id: Math.floor(Math.random()*10000),
         username : name,
         userphone : phone
     }
     record.push(obj);
     console.log("Successfully added..!");
+    return res.redirect('/');
+})
+
+app.get('/edituser',(req,res)=>{
+    let id = req.query.editId;
+    let singleData = record.find(val => val.id == id);
+    return res.render('edit',{
+        singleData
+    })
+})
+
+app.post('/editData',(req,res) => {
+    const {editId, username, userphone, useremail, userpassword, usertask, userstatus, userdeadline} = req.body;
+    let up = record.map((val) => {
+        if(val.id == editId){
+            val.username = name,
+            val.userphone = phone
+        }
+        return val;
+    })
+    record = up;
+    console.log("successfully updated..!");
     return res.redirect('/');
 })
 
