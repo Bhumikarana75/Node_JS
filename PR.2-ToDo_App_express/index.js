d086b771fe2c56cc8ea8cf51348dec72e32eec55
Bhumikarana75 <bhumikarana958@gmail.com>
2025-01-02 10:55:24 +0530

    Node JS

--- PR.2-ToDo_App_express/index.js
+++ PR.2-ToDo_App_express/index.js
@@ -1,60 +1,72 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-
 const path = require('path');
+
 app.set('view engine', 'ejs');
-app.use('/',express.static(path.join(__dirname,'assets')));
+app.use('/', express.static(path.join(__dirname, 'assets')));
 app.use(express.urlencoded());
+
 let record = [];
 
-
-app.get('/', (req,res) => {
-    return res.render('table',{
+app.get('/', (req, res) => {
+    return res.render('table', {
         record
     });
-})
-app.get('/add', (req,res) => {
-    return res.render('add');
-}); 
+});
 
-app.post('/adduser',(req,res) => {
-    const {name, phone} = req.body;
+app.get('/add', (req, res) => {
+    return res.render('add');
+});
+
+app.post('/adduser', (req, res) => {
+    const { username, usertask, userstatus, userdeadline } = req.body;
     let obj = {
-        id: Math.floor(Math.random()*10000),
-        username : name,
-        userphone : phone
-    }
+        id: Math.floor(Math.random() * 10000),
+        username,
+        usertask,
+        userstatus, 
+        userdeadline
+    };
     record.push(obj);
     console.log("Successfully added..!");
     return res.redirect('/');
-})
+});
 
-app.get('/edituser',(req,res)=>{
+app.get('/edituser', (req, res) => {
     let id = req.query.editId;
     let singleData = record.find(val => val.id == id);
-    return res.render('edit',{
+    return res.render('edit', {
         singleData
     })
 })
 
-app.post('/editData',(req,res) => {
-    const {editId, username, userphone, useremail, userpassword, usertask, userstatus, userdeadline} = req.body;
+app.post('/editData', (req, res) => {
+    const { editId, username, usertask, userstatus, userdeadline } = req.body;
     let up = record.map((val) => {
-        if(val.id == editId){
-            val.username = name,
-            val.userphone = phone
+        if (val.id == editId) {
+            val.username = username;
+            val.usertask = usertask;
+            val.userstatus = userstatus;
+            val.userdeadline = userdeadline;
         }
         return val;
-    })
+    });
     record = up;
-    console.log("successfully updated..!");
+    console.log("Successfully updated..!");
     return res.redirect('/');
-})
+});
+
+app.get('/deleteUser', (req, res) => {
+    const { delId } = req.query;
+    record = record.filter(user => user.id != delId); // Remove user by id
+    console.log("User deleted successfully!");
+    return res.redirect('/'); // Redirect back to the main page
+});
 
 app.listen(port, (err) => {
-    if(err){
+    if (err) {
         console.log('Error in running the server', err);
     }
-    console.log(`Server is running on port https://localhost:${port}`);
+    console.log(`Server is running on port http://localhost:8080`);
 })
