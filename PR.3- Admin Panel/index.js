ebe3ca1fc997fa4b38301386f8695db441d4400b
Bhumikarana75 <bhumikarana958@gmail.com>
2025-01-10 19:24:47 +0530

    Node JS

--- PR.3- Admin Panel/index.js
+++ PR.3- Admin Panel/index.js
@@ -8,6 +8,24 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req,res) =>{
     return res.render('dashboard');
 })
+app.get('/charts', (req,res) =>{
+    return res.render('charts');
+})
+app.get('/widgets', (req,res) =>{
+    return res.render('widgets');
+})
+app.get('/tables', (req,res) =>{
+    return res.render('tables');
+})
+app.get('/grid',(req,res) =>{
+    return res.render('grid');
+})
+app.get('/form-basic',(req,res) =>{
+    return res.render('form');
+})
+app.get('/pages-buttons',(req,res) =>{
+    return res.render('pages-buttons');
+})
 
 app.listen(port, (err) => {
     if (err) {
